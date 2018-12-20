0af675b4c302c8d53079706b77e13cc8ae24c5c7
TeroJokela <tero.jokela10@gmail.com>
2018-12-20 20:06:59 +0200

    Added earrape command

--- cogs/troll.py
+++ cogs/troll.py
@@ -1,8 +1,10 @@
 from discord.ext import commands
 from .helpers import checks
 import discord
+import os
 
 
+# No error handlers for these commands, because I want these to be "secret"
 class Troll(object):
     def __init__(self, client: commands.Bot):
         self.client = client
@@ -14,6 +16,24 @@ class Troll(object):
         await self.client.delete_message(ctx.message)
         msg = await self.client.say(toSay, tts=True)
         await self.client.delete_message(msg)
+
+    def leaveVoice(self, _voice: discord.VoiceClient):
+        coro = _voice.disconnect()
+        fut = discord.compat.run_coroutine_threadsafe(coro, self.client.loop)
+        fut.result()
+
+    @commands.command(pass_context=True, brief="[channel ID]")
+    @commands.check(checks.isCreator)
+    async def earrape(self, ctx: commands.Context, channelID: str):
+        """Earrape everyone in a voice channel"""
+        channel = self.client.get_channel(channelID)
+        if channel.type != discord.ChannelType.voice:
+            return
+        voice = await self.client.join_voice_channel(channel)
+        player = voice.create_ffmpeg_player(f"{os.getcwd()}/resources/earrape.mp3", after=lambda: self.leaveVoice(voice))
+        player.volume = 2.0
+        player.start()
+        await self.client.delete_message(ctx.message)
 
 
 def setup(client: commands.Bot):
